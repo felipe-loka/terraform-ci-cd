699ed1fb4f7a4987d34a4b48965d1a3033a42f07
felipe-loka <felipe@loka.com>
2025-03-07 16:08:14 -0300

    fix security issues

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,7 @@
 resource "aws_instance" "myec2" {
   ami           = "ami-05b10e08d247fb927"
-  instance_type = "t1.2xlarge"
+  instance_type = "t3.micro"
+  metadata_options {
+    http_tokens = "required"
+  }
 }
