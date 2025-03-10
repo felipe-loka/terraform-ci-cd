080fb91db6e7f2342b0d58bc2a7552de62781cac
felipe-loka <felipe@loka.com>
2025-03-10 13:28:01 -0300

    add ec2 module

--- main.tf
+++ main.tf
@@ -3,3 +3,9 @@ resource "aws_sqs_queue" "terraform_queue" {
   name                    = var.queue_name
   sqs_managed_sse_enabled = true
 }
+
+
+module "ec2" {
+  source        = "./modules/ec2"
+  instance_type = "t3.minix"
+}
